ae4cfb6692a07606063d7713605a5f88618c203b
Tamsin Coton <tamcoton@hotmail.co.uk>
2020-08-27 15:36:39 +0100

    Logging

--- module/utils.js
+++ module/utils.js
@@ -1,6 +1,7 @@
 export class Utils {
     static sendCustomEvent(data) {
         const event = new CustomEvent("UDAW", { "detail": data });
+        console.log("dispatching event:", event);
         document.dispatchEvent(event);
     }
 }
